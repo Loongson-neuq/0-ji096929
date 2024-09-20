30d6044c46c9e3ea73b089b5c3f9ba59e1e8bbcf
ji096929 <2455742327@qq.com>
2024-09-20 19:40:19 +0800

    完成作业

--- main.cpp
+++ main.cpp
@@ -4,4 +4,17 @@
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
+    int x = a, y = b, z = c;
+
+    if (x > y)
+        std::swap(x, y);
+    if (y > z)
+        std::swap(y, z);
+    if (x > y)
+        std::swap(x, y);
+
+    result.min = x;
+    result.mid = y;
+    result.max = z;
 }
+
